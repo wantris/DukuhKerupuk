4046e6fb0815176eab1a3619236cd3c529ce5fd3
wantris <gusiwacik@gmail.com>
2021-02-26 15:50:56 +0700

    voucher

---
+++ database/migrations/2021_02_26_072754_create_voucher_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVoucherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('code', 15);
+            $table->text('description');
+            $table->date('expired_date');
+            $table->char('status', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+}
